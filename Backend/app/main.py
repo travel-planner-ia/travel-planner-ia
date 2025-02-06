81965be527b7598919b5cd94eae2601329d7be38
Estanislao Pan García <estanislao.pan@viewnext.com>
2025-02-06 16:45:13 +0100

    Recepción de request en el back

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from typing import List
 from models.user_request import Datos
 from fastapi.middleware.cors import CORSMiddleware
 from services.list_of_countries import list_of_countries
 from services.stream_answer import stream_answer, procesar_respuesta;
+import json
+
 # from services.get_countries import return_countries;
 # import bs4
 #print(bs4.__version__)
@@ -33,13 +35,20 @@ async def get_home():
 
 # Ruta para enviar consultas al servidor
 @app.post("/servidor")
-async def post_to_servidor(datos: Datos):
-    #"datos" es lo que nos llega desde el formulario de front
-    print("datos =", datos)
+async def post_to_servidor(frontRequest: Datos):
+    #"frontRequest" es lo que nos llega desde el formulario de front
+    print("frontRequest =", frontRequest)
+
+    json_de_data = frontRequest.dict(exclude_unset=True)
+    print("json_de_data =", json_de_data)
+
     #Aquí haremos la llamada
 
     #Respuesta de la llamada
 
     #Esto es lo que retornaremos al front
-    #procesar_respuesta()
-    return {"mensaje": "Bienvenido al servidor", "respuesta":procesar_respuesta()}
+    #La función procesar_respuesta() nos devuelve los datos
+    return {
+        "mensaje": "Bienvenido al servidor", 
+        "respuesta":procesar_respuesta()
+        }
