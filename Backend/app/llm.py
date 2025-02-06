5469a2320e952dbaa4956882c93e60409fcf4bb9
DAVID MAKOVETSKIY MAKOVETSKA <dm655@inlumine.ual.es>
2025-02-06 21:38:46 +0100

    Empleo de semaforo en main y correccion del prompt final

--- Backend/app/llm.py
+++ Backend/app/llm.py
@@ -14,12 +14,16 @@ class LLM:
             self.temperature = 0
             self.respuesta = ''
     
-    def _prompting(self,rag_response:str,amadeus_response:str,formulario:str):
+    def _prompting(self, rag_response:str, amadeus_response:str, formulario:str):
+          
+          # Convertir rag_response en un string legible
+          rag_text = "\n".join([f"- **{item['query']}**: {item['response']}" for item in rag_response])
+
           prompt = dedent(f"""
             Actúa como un experto asistente de viajes y guía turístico especializado en recomendaciones para viajes familiares. A continuación, te proporciono información clave:  
 
             1. **Recomendaciones previas basadas en el contexto:**  
-            {rag_response}  
+            {rag_text}  
 
             2. **Información estructurada sobre vuelos, hoteles y lugares de interés:**  
             {amadeus_response}  
@@ -36,11 +40,18 @@ class LLM:
             - Recomendaciones sanitarias, restricciones y consejos sobre transporte, movilidad y seguridad en el país
             Responde de manera clara, directa y organizada para facilitar la planificación del viaje familiar.  
             """)
+          
           return prompt
     
-    def _llamada_llm(self,query_final):
+    def _llamada_llm(self,query_final, messages=[]):
+        messages.append(
+            {
+                "role": "user",
+                "content": query_final,
+            }
+        )
         response = self.client.chat.completions.create(
-                model=self.model, messages=query_final, temperature=self.temperature
+                model=self.model, messages=messages, temperature=self.temperature
             )
         return response.choices[0].message.content
           
